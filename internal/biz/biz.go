8c1d2f0f3dcae5cf876f30aa55b3b966dfe21a9f
wangxinhao <2571854116@qq.com>
2026-07-31 14:42:59 +0800

    chore: ignore frontend dependencies

--- internal/biz/biz.go
+++
@@ -1,6 +0,0 @@
-package biz
-
-import "github.com/google/wire"
-
-// ProviderSet is biz providers.
-var ProviderSet = wire.NewSet(NewTodoUsecase, NewAddressUsecase, NewUserUsecase, NewOrderUsecase, NewChannelUsecase, NewLockerUsecase, NewServicePointUsecase, NewPickupUsecase, NewFreightUsecase, NewAuthUsecase, NewExpressOrderUsecase)
